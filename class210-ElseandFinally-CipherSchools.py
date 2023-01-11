919d821b9a53119d4a13e0e532d17e0175b4950c
khushi jaiswal <119439224+khushi-jaiswal0512@users.noreply.github.com>
2023-01-12 02:15:29 +0530

    Add files via upload

---
+++ class210-ElseandFinally-CipherSchools.py
@@ -0,0 +1,17 @@
+while True:
+    try :
+        number = int(input("enter a new number : "))
+    except ValueError:
+        print("Please type integer!! s")
+    except:
+        print("Unexpected error!!!")
+    else:
+        print(f'user input = {number}')
+        break
+    finally:
+        print('finally blocks....')
+        
+        
+    
+    
+    
